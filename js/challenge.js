4607145a43098810a666988535c50d32133a1a13
Christopher Carrasco <christopher3carrasco@gmail.com>
2022-12-28 17:32:27 -0500

    feat: add decryption

--- js/challenge.js
+++ js/challenge.js
@@ -10,6 +10,7 @@ const CLAVES = {
 
 let input = document.querySelector("textarea");
 let btnEncrypt = document.querySelector("#encriptar");
+let btnDencrypt = document.querySelector("#desencriptar");
 
 const encrypt = () => {
   if (input.value == "") {
@@ -41,4 +42,29 @@ const copypaste = () => {
   navigator.clipboard.writeText(msg);
 };
 
+const dencrypt = () => {
+  if (input.value == "") {
+    document.getElementById(
+      "clave"
+    ).innerHTML = `<h5>Ningún mensaje fue encontrado</h5><p>Ingresa un mensaje para encriptar o desencriptar.</p>`;
+    return;
+  }
+
+  let str = input.value;
+
+  let msg = str
+    .replace(CLAVES.a, "a")
+    .replace(CLAVES.e, "e")
+    .replace(CLAVES.i, "i")
+    .replace(CLAVES.o, "o")
+    .replace(CLAVES.u, "u");
+
+  document.getElementById(
+    "clave"
+  ).innerHTML = `<p>${msg}</p><div class="col s12 btn-d"><a id="copiar" class="waves-effect waves-light btn-large btn-s" onclick="copypaste()">Copiar</a></div>`;
+
+  input.value = "";
+};
+
 btnEncrypt.onclick = encrypt;
+btnDencrypt.onclick = dencrypt;
